e021f1dbf23bfd5f1f17272ac622c2d078dc32f2
manishp608 <manishpalavalasa@gmail.com>
2026-03-03 16:03:04 +0530

    Login from database working properly

--- backend/authServer.js
+++ backend/authServer.js
@@ -13,7 +13,7 @@ const jwt = require('jsonwebtoken')
 
 app.use(express.json())
 
-app.use(cors({origin: "http://127.0.0.1:5500"}));
+app.use(cors({origin: "http://127.0.0.1:60333"}));
 
 let refreshTokens = []
 
