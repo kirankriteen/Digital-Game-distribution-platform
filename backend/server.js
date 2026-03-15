66b66e4507368d105fec6479a104d7c235ac30d7
manishp608 <manishpalavalasa@gmail.com>
2026-03-15 17:31:44 +0530

    Made the frontend static dir
    Restricting access needs reordering of folders or can be done using frontend\n will look into it later

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt')
 const cors = require('cors')
 const PORT = 3000
 const app = express()
+const path = require("path")
 const { ROLE, users, projects } = require('./data')
 const { authUser, authRole } = require('./roleAuth')
 const { authenticateToken, setUser } = require('./middleware/auth')
@@ -13,7 +14,19 @@ const gamesRouter = require('./routes/games')
 const devRouter = require('./routes/developer')
 
 const jwt = require('jsonwebtoken')
-app.use(cors({ origin: "http://127.0.0.1:60729" }));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+
+// const notAllowedExt = ['.htm', '.html']
+// app.use((req, res, next) => {
+//   const ext = path.extname(req.path).toLowerCase();
+//   if (ext && notAllowedExt.includes(ext)) {
+//     return res.status(404).send('Not allowed');
+//   }
+
+//   next();
+// });
+app.use(express.static(path.join(__dirname, "../frontend")))
 
 app.use(express.json())
 app.use('/projects', projectRouter)
@@ -21,7 +34,9 @@ app.use('/games', gamesRouter)
 app.use('/dev', devRouter)
 
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    // res.send('Home Page')
+    // res.sendFile(path.join(__dirname, "../frontend/pages/signup_login.htm"));
+    res.sendFile(path.join(__dirname, "../frontend/base.html"))
 })
 
 app.get('/admin', authenticateToken, setUser, authRole(ROLE.ADMIN), (req, res) => {
